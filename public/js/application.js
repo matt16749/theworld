8dbd2de049da49e299ad0e5881940aff74c940f2
Matt <matt16749@gmail.com>
2014-08-18 08:57:05 -0700

    adds imagery layers to Cesium file application.js need to provide notes and documentation

--- public/js/application.js
+++ public/js/application.js
@@ -1,7 +1,19 @@
-$(document).ready(function() {
-  // This is called after the document has loaded in its entirety
-  // This guarantees that any elements we bind to will exist on the page
-  // when we try to bind to them
-
-  // See: http://docs.jquery.com/Tutorials:Introducing_$(document).ready()
+var viewer = new Cesium.Viewer('cesiumContainer', {
+    imageryProvider : new Cesium.ArcGisMapServerImageryProvider({
+        url : 'http://server.arcgisonline.com/ArcGIS/rest/services/World_Street_Map/MapServer'
+    }),
+    baseLayerPicker : false
 });
+
+var layers = viewer.scene.imageryLayers;
+var blackMarble = layers.addImageryProvider(new Cesium.TileMapServiceImageryProvider({
+    url : '//cesiumjs.org/tilesets/imagery/blackmarble',
+    maximumLevel : 8,
+    credit : 'Black Marble imagery courtesy NASA Earth Observatory'
+}));
+
+blackMarble.alpha = 0.5;
+
+
+// Default
+// var viewer = new Cesium.Viewer('cesiumContainer');
